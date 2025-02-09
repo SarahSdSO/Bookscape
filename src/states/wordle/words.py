72971f62399fdca45a7f0a8a7c1b6031b495725c
Sarah dos Santos Oliveira <sarinhasaracura2@gmail.com>
2025-02-09 18:40:14 -0300

    Wordle

---
+++ src/states/wordle/words.py
@@ -0,0 +1,7 @@
+WORDS = [
+    "amigo", "autor", "livro", "capas", "citar", "vento", "olhos"
+    "copiar", "dados", "drama", "eixos", "ideia", "lucro", "justo"
+    "notas", "obras", "papel", "poeta",  "magia", "matar", "ponte",
+    "temas", "texto", "dizer", "estar", "fazer", "beijo", "honra"
+]
+    
