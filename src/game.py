8f848d96c9d240d71726fa4f9efe6559ba2b8702
Erika <erikaliz2014augusto@gmail.com>
2025-02-09 18:19:32 -0300

    commit correction

--- src/game.py
+++ src/game.py
@@ -1,6 +1,5 @@
 import pygame
 from states.menuState import MenuState
-from states.ticTacToeState import TicTacToeState
 
 class Game:
     def __init__(self):
@@ -21,7 +20,7 @@ class Game:
         self.icon = pygame.image.load("assets/icons/gameIcon.png")
         pygame.display.set_icon(self.icon)
         
-        self.state = TicTacToeState(self)
+        self.state = MenuState(self)
 
     def change_state(self, new_state):
         self.state = new_state
